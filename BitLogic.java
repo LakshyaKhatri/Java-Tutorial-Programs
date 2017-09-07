79e5af4371868113007c997bfb5aeac00aedbf17
LakshyaKhatri <lakshyak67@gmail.com>
2017-09-07 23:22:22 +0530

    Add program to understand bitwise operators. Incomplete yet !

---
+++ BitLogic.java
@@ -0,0 +1,14 @@
+class BitLogic 
+{
+	public static void main(String[] args)
+	{
+		String binary[] = {"0000","0001","0010","0011","0100","0101","0110","0111","1000","1001","1010","1011","1100","1101","1110","1111"};
+		int a = 3;  // 0011 in binary
+		int b = 6; // 0110 in binary
+		int c = a | b;
+		int d = a & b;
+		int e = a ^ b;
+		int f = (~a & b)||(a & ~b);
+		int g = -a & 0x0f;
+	}
+}
