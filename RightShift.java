943007194d5fc99fb9e27a8172f931e3c5db769f
LakshyaKhatri <lakshyak67@gmail.com>
2017-09-08 21:01:13 +0530

    Add program to understand Right Shift operator " >> "

---
+++ RightShift.java
@@ -0,0 +1,10 @@
+class RightShift
+{
+	public static void main(String[] args)
+	{
+		char hex[] = { '0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
+		byte b = (byte) 0x0f;
+
+		System.out.println("b = 0x" + hex[(b >> 4) & 0x0f] + hex[b & 0x0f]);
+	}
+}
