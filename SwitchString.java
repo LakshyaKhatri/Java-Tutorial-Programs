edbb6e8c0e2b08bbfec459e19b373cd2fc9dc8cc
LakshyaKhatri <lakshyak67@gmail.com>
2017-09-09 19:46:39 +0530

    Add program to understand Strings as cases in "switch"

---
+++ SwitchString.java
@@ -0,0 +1,22 @@
+class SwitchString
+{
+	public static void main(String[] args)
+	{
+		String choice = "two";
+		switch(choice)
+		{
+			case "one":
+				System.out.println(" Choice is one ");
+				break;
+			case "two":
+				System.out.println(" Choice is two ");
+				break;
+			case "three":
+				System.out.println(" Choice is three ");
+				break;
+			case "four":
+				System.out.println(" Choice is four ");
+				break;
+		}
+	}
+}
